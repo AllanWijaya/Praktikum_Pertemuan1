c1d17e552d2eeff39bdee857757d5a4c554ccc41
Allan Wijaya <allangaulmen7@gmail.com>
2021-11-04 14:26:24 +0700

    second commit

--- src/modul107350/Modul107350.java
+++ src/modul107350/Modul107350.java
@@ -1,58 +1,58 @@
 package modul107350;
 import java.util.Scanner;
-class konversi07350
+class konversi_07350
 {
-    public void choice07350(float hasil07350)
+    public void choice_07350(float hasil_07350)
     {
-        if(hasil07350<60)
+        if(hasil_07350<60)
         {
             System.out.println(">>Nilai anda adalah E");
         }
-        else if(hasil07350>=60 && hasil07350<70)
+        else if(hasil_07350>=60 && hasil_07350<70)
         {
             System.out.println(">>Nilai anda adalah D");
         }   
-        else if(hasil07350>=70 && hasil07350<80)
+        else if(hasil_07350>=70 && hasil_07350<80)
         {
             System.out.println(">>Nilai anda adalah C");
         }
-        else if(hasil07350>=80 && hasil07350<90)
+        else if(hasil_07350>=80 && hasil_07350<90)
         {
             System.out.println(">>Nilai anda adalah B");
         }
-        else if(hasil07350>=90 && hasil07350<=100)
+        else if(hasil_07350>=90 && hasil_07350<=100)
         {
             System.out.println(">>Nilai anda adalah A");
         }
     }
 }
-class arraynilai07350
+class arraynilai_07350
 {
-    public void tes07350()
+    public void tes_07350()
     {
-        float yahaha07350 = 0,tes07350 = 0;
-        konversi07350 objnil07350 = new konversi07350();
-        Scanner input07350 = new Scanner(System.in);
+        float yahaha_07350 = 0,tes_07350 = 0;
+        konversi_07350 objnil_07350 = new konversi_07350();
+        Scanner input_07350 = new Scanner(System.in);
         System.out.print("Masukkan jumlah nilai yang akan diinputkan = ");
-        int banyak07350 = input07350.nextInt();
-        float[] array07350 = new float[banyak07350];
-        for(int i=0;i<banyak07350;i++)
+        int banyak_07350 = input_07350.nextInt();
+        float[] array_07350 = new float[banyak_07350];
+        for(int i=0;i<banyak_07350;i++)
         {
             System.out.print("Masukkan nilai ke-"+i+" = ");
-            array07350[i]=input07350.nextInt();
-            objnil07350.choice07350(array07350[i]);
+            array_07350[i]=input_07350.nextInt();
+            objnil_07350.choice_07350(array_07350[i]);
         }
-        for(int j=0;j<banyak07350;j++)
+        for(int j=0;j<banyak_07350;j++)
         {
-            tes07350=tes07350+array07350[j];
+            tes_07350=tes_07350+array_07350[j];
         }
-        tes07350=tes07350/banyak07350;
-        System.out.println("Rata-Rata Seluruh Nilai adalah "+tes07350);
-        if(tes07350<75)
+        tes_07350=tes_07350/banyak_07350;
+        System.out.println("Rata-Rata Seluruh Nilai adalah "+tes_07350);
+        if(tes_07350<75)
         {
             System.out.println("===ANDA TIDAK LULUS===\n");
         }
-        else if(tes07350>=75)
+        else if(tes_07350>=75)
         {
             System.out.println("===ANDA LULUS===\n");
         }
@@ -62,21 +62,21 @@ public class Modul107350
 {
     public static void main(String[] args) 
     {
-        int tes207350;
-        Scanner menuin07350 = new Scanner(System.in);
-        arraynilai07350 objarr07350 = new arraynilai07350();
+        int tes2_07350;
+        Scanner menuin_07350 = new Scanner(System.in);
+        arraynilai_07350 objarr_07350 = new arraynilai_07350();
         do
         {
             System.out.println("Konversi Nilai Rata-Rata Mata Pelajaran");
             System.out.println("1. Cek Nilai");
             System.out.println("2. Keluar");
             System.out.print("Inputkan menu = ");
-            tes207350=menuin07350.nextInt();
-            switch(tes207350)
+            tes2_07350=menuin_07350.nextInt();
+            switch(tes2_07350)
             {
                 case 1:
                 {
-                    objarr07350.tes07350();
+                    objarr_07350.tes_07350();
                     break;
                 }
                 case 2:
@@ -88,7 +88,7 @@ public class Modul107350
                     System.out.println("Nilai yang anda inputkan salah");
                 }     
             }
-        }while(tes207350==1);
+        }while(tes2_07350==1);
     }
     
 }
